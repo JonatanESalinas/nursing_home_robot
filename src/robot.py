aa2c248a042a104f049f91cf1f330eb83f02e8cd
JonatanESalinas <A01731815@itesm.mx>
2021-09-13 17:13:03 -0500

    Reacomodo del codigo para ser mas legible y modular
    En este commit:
    En el main.py:
    -el menu se paso a una funcion externa y se invoca desde el main.
    -se cambio el nombre del nodo de "nodo_robot" a "nodo_main"
    -se quitaron las lineas del actionlib message correspondiente al goal, y se pusieron
    en una funcion dentro de la clase Robot.
    -se creo una variable "proximoObjetivo" para poner ahi temporalmente la coordenada.
    -se quitaron las lineas que crean al cliente de la accion move_base.
    
    En robot.py
    -se pusieron en el constructor las lineas del cliente de la action move_base
    -se agrego una funcion "ve_a_habitacion" que recibe coordenadas, crea el nuevo goal y lo manda
    al servidor de la accion move_base.

--- src/robot.py
+++ src/robot.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python
 import os
+import rospy
+import actionlib
+from actionlib_msgs.msg import *
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import Point
 
 class Robot:
     
-    #def __init__(self):
+    def __init__(self):     
+        self.clienteAccionBase = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
+        rospy.loginfo("Esperando al action server move_base...")
+        self.clienteAccionBase.wait_for_server()
+        rospy.loginfo("Action server move_base identificado y listo.")
+
+    def ve_a_habitacion(self, coordenada_x, coordenada_y):
+        goal = MoveBaseGoal()
+
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.stamp = rospy.Time.now()
+
+        goal.target_pose.pose.position =  Point(coordenada_x, coordenada_y, 0)
+
+        goal.target_pose.pose.orientation.x = 0.0
+        goal.target_pose.pose.orientation.y = 0.0
+        goal.target_pose.pose.orientation.z = 0.0
+        goal.target_pose.pose.orientation.w = 1.0
+
+        rospy.loginfo("")
+        self.clienteAccionBase.send_goal(goal)
 
     def decir_hola_hora_medicina(self):
         os.system("mpg321 ../voice/medicina_only.mp3")
